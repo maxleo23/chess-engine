f5de28be3e48d175234375eade68dc9defeac7f7
maxleo23 <lesandabandara0@gmail.com>
2026-09-15 13:25:10 +0530

    Fix board initialization

--- defs.h
+++ defs.h
@@ -1,6 +1,41 @@
 #ifndef defs_H
 #define defs_H
 
+#include "stdlib.h"
+
+// For Debug --- Found in a Book "Begining For The C++"
+
+#define DEBUG
+
+#ifndef DEBUG
+#define ASSERT(n)
+#else
+#define ASSERT(n) \
+    if (!(n)) { \
+        printf("%s - Failed", #n); \
+        printf("On %s", __DATE__); \
+        printf("At %s", __TIME__); \
+        printf("In FILE %s", __FILE__); \
+        printf("At LINE %d\n", __LINE__); \
+        exit(1); \
+    }
+#endif
+
+/*
+#ifndef DEBUG
+#define ASSERT(n)
+#else
+#define ASSERT(n) \
+    if (!(n)) { \
+        printf("%s - Failed\n", #n); \
+        printf("On %s\n", __DATE__); \
+        printf("At %s\n", __TIME__); \
+        printf("In FILE %s\n", __FILE__); \
+        printf("At LINE %d\n", __LINE__); \
+        exit(1);
+    }
+#endif  */
+
 typedef unsigned long long U64;
 
 #define NAME "Name"
@@ -65,21 +100,41 @@ typedef struct {
     
     S_UNDO history[MAXGAMEMOVES];
 
+    // Piece List
+    int pList[13] [10];
+
+
+    /*  pList[wN] [0] = E1;
+        pList[wN] [1] = D4;
+
+     */
 } S_BOARD ;
 
 
 /* MACROS */
 
 #define FR2SQ(f, r) ( ( 21 + (f) + ( (r) * 10 ) ) )
+#define SQ64(sq120) Sq120ToSq64[sq120]
+#define POP(b) PopBit(b)
+#define CNT(b) CountBits(b)
+#define CLRBIT(bb, sq) ((bb) &= ClearMask[(sq)])
+#define SETBIT(bb, sq) ((bb) |= SetMask[(sq)])
 
 /* GLOBALS */
 
 extern int Sq120ToSq64[BRD_SQ_NUM];
 extern int Sq64ToSq120[64];
+extern U64 SetMask[64];
+extern U64 ClearMask[64];
 
 /* FUNCTIONS */
 
+// init.c 
 extern void AllInit();
 
+// bitboards.c
+extern void PrintBitBoard(U64 bb);
+extern int PopBit(U64 *bb);
+extern int CountBits(U64 b);
 
 #endif  
--- vice.c
+++ vice.c
@@ -1,24 +1,18 @@
 #include "stdio.h"
-#include "defs.h" 
+#include "defs.h"
 
-int main() {
-    
+int main(void)
+{
+
     AllInit();
-    
     int index = 0;
+    U64 playBitBoard = 0ULL;
 
-    for ( index = 0; index < BRD_SQ_NUM; ++index ) {
-        if(index%10==0) printf("\n");
-        printf("%5d" ,Sq120ToSq64[index]);    
-    }   
-
-    printf("\n");
-    printf("\n");
-
-    for ( index = 0; index < 64; ++index ) {
-        if(index%8==0) printf("\n");
-        printf("%5d" ,Sq64ToSq120[index]);    
-    }
+    SETBIT(playBitBoard, 61);
+    PrintBitBoard(playBitBoard);
+    
+    CLRBIT(playBitBoard, 61);
+    PrintBitBoard(playBitBoard);
 
     return 0;
-}   
+}
